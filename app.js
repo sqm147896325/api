ee3b9e9b168f54652098948a2e4648349656bbe8
孙启萌 <hb.sunqimeng@aisino.com>
2021-05-28 18:04:20 +0800

    增加app启动回调

---
+++ app.js
@@ -0,0 +1,11 @@
+module.exports = app => {
+    // 开始前执行
+    app.beforeStart(async () => {
+    });
+    // 准备好执行
+    app.ready(async () => {
+    });
+    // 关闭前执行
+    app.beforeClose(async () => {
+    });
+};
