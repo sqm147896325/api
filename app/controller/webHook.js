b579f1b903ef729333692d8e86990a668e94cc16
sqm147896325 <1435585893@qq.com>
2021-07-03 23:48:39 +0800

    持续集成完成

--- app/controller/webHook.js
+++ app/controller/webHook.js
@@ -1,10 +1,6 @@
 'use strict';
 // 生成子进程
 const child = require('child_process');
-// 脚本地址
-const script = '~/server/api/script/autoDeploy.sh';
-// 执行指令的地址
-const currentPath = '~/server/api/';
 
 const Controller = require('egg').Controller;
 
@@ -21,8 +17,8 @@ class ApiController extends Controller {
 		const { helper,params } = ctx;
 		console.log('params',params);
 		if(ctx.headers['x-gitee-token'] === process.env.SQL_PASSWORD){
-			child.exec( 'sh ~/server/api/script/autoDeploy.sh', (err) => {
-				console.log(err);
+			child.exec( 'sh ~/server/api/script/autoDeploy.sh', (err,sto) => {
+				helper.success('部署成功',sto);
 			})
 			helper.success('部署成功');
 		}else{
