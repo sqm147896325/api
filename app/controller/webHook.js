5ed2792a3cbc68dd8cf2c17bdc8a060efede333e
sqm147896325 <1435585893@qq.com>
2021-07-03 23:14:21 +0800

    修改

--- app/controller/webHook.js
+++ app/controller/webHook.js
@@ -2,9 +2,9 @@
 // 生成子进程
 const spawn = require('child_process').spawn;
 // 脚本地址
-const script = '/home/sqm/server/api/script/autoDeploy.sh';
+const script = '~/server/api/script/autoDeploy.sh';
 // 执行指令的地址
-const currentPath = '/home/sqm/server/api/';
+const currentPath = '~/server/api/';
 
 const Controller = require('egg').Controller;
 
