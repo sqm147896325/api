95188b7eb7f62cc39633b7e3371d41dac482068d
sqm147896325 <1435585893@qq.com>
2021-07-04 11:37:13 +0800

    部署输出错误

--- app/controller/webHook.js
+++ app/controller/webHook.js
@@ -18,9 +18,12 @@ class ApiController extends Controller {
 		console.log('params',params);
 		if(ctx.headers['x-gitee-token'] === process.env.SQL_PASSWORD){
 			child.exec( 'sh ~/server/api/script/autoDeploy.sh', (err,sto) => {
-				helper.success('部署成功',sto);
-			})
-			helper.success('部署成功');
+				if(err){
+					helper.fail('部署失败',err);
+				}else{
+					helper.success('部署成功',sto);
+				}
+			});
 		}else{
 			console.log('params',params);
 			helper.success('密钥错误');
