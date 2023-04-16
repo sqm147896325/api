8f706eb47e70e47ed467b0b104cff4fb474a133e
sunqimeng <1435585893@qq.com>
2023-04-16 20:12:14 +0800

    测试自动打包

--- script/deployBack.sh
+++ script/deployBack.sh
@@ -1,7 +1,7 @@
 # 切换目录
 cd project
 # 判断目录是否存在
-if [ ! -d "/home/sqm/server/api/project/blog-back" ]; then
+if [ ! -d "./blog-back" ]; then
     git clone https://gitee.com/sqm147896325/blog-back.git
     cd blog-back
 else
@@ -10,6 +10,6 @@ else
 fi
 # ! 由于未知原因，使用脚本运行yarn相关命令会宕机，这里仅自动更新，需要手动升级
 # # 安装依赖
-# yarn
+yarn
 # # 打包
-# yarn build
+yarn build
